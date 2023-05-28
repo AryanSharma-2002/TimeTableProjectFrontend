a1d887d974f0ba5d40abb60a734c4dc1ef9f5286
Aryan Sharma <aryanmohit52@gmail.com>
2023-05-28 19:48:26 +0530

    second commit

--- src/base_url.js
+++ src/base_url.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = "http://127.0.0.1:5000";
-export const BASE_URL = "https://timetableprojectbackend.onrender.com";
+// export const BASE_URL = "https://timetableprojectbackend.onrender.com";
+export const BASE_URL =
+  "https://timetableprojectbackend.aryansharma37.repl.co/";
